863767f10263037cbd1cb86d044e38bcc3eb8ca3
Filipe Soares <filipecarlossoares@gmail.com>
2021-12-11 10:25:40 -0300

    move the files to the right place

--- test/unit/Order.test.ts
+++ test/unit/Order.test.ts
@@ -1,8 +1,8 @@
-import Coupon from "../src/Coupon";
-import DefaultFreightCalculator from '../src/DefaultFreightCalculator';
-import FixedFreightCalculator from '../src/FixedFreightCalculator';
-import Item from "../src/Item";
-import Order from "../src/Order";
+import Coupon from "../../src/domain/entity/Coupon";
+import DefaultFreightCalculator from '../../src/domain/entity/DefaultFreightCalculator';
+import FixedFreightCalculator from '../../src/domain/entity/FixedFreightCalculator';
+import Item from "../../src/domain/entity/Item";
+import Order from "../../src/domain/entity/Order";
 
 test("Should create an order with valid cpf", function () {
 	const cpf = "839.435.452-10";
